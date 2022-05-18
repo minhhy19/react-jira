843ad962e0e793021aaeee6f623796857ae01f6e
Pham Minh Hy <hypm@MECORP.LOCAL>
2022-05-18 17:19:43 +0700

    update

--- src/templates/HomeTemplate/HomeTemplate.jsx
+++ src/templates/HomeTemplate/HomeTemplate.jsx
@@ -1,23 +1,50 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import MenuJira from './Layouts/MenuJira/MenuJira';
 import ModalJira from './Layouts/ModalJira/ModalJira'
 import SidebarJira from './Layouts/SidebarJira/SidebarJira';
+import _ from 'lodash';
 
 import '../../index.css';
+import { USER_LOGIN } from '../../util/constants/settingSystem';
+import { messageApp } from '../../util/Common/Message';
+import { Redirect } from 'react-router-dom';
+import { notificationFunction } from '../../util/Notification/notificationJira';
 
 export const HomeTemplate = (props) => {
+    const { messageAuthorization } = messageApp;
+
+    let userInfo = localStorage.getItem(USER_LOGIN);
+    if (!_.isEmpty(userInfo) && !_.isNull(userInfo)) {
+        userInfo = JSON.parse(userInfo);
+    }
+
+    const handleRender = () => {
+        switch (true) {
+            case !userInfo: {
+                // notificationFunction('warning', messageAuthorization);
+                return <Redirect to='/login' />;
+            }
+
+            default:
+                return null;
+        }
+    };
 
     const { Component, ...restParam } = props;
-    return <Route {...restParam} render={(propsRoute) => {
-        return <>
-            <div className="jira">
-                <SidebarJira />
-                <MenuJira />
-                    <Component {...propsRoute} />
-                <ModalJira />
-            </div>
+    return (
+        <>
+            {handleRender() || <Route {...restParam} render={(propsRoute) => {
+                return <>
+                    <div className="jira">
+                        <SidebarJira />
+                        <MenuJira />
+                        <Component {...propsRoute} />
+                        <ModalJira />
+                    </div>
+                </>
+            }} />}
         </>
-    }} />
+    );
 
 }
---
+++ src/util/Common/Message.jsx
@@ -0,0 +1,102 @@
+const messagePasswordIsValid =
+  '*Passwords of at least eight (8) characters or more (longer is better). Use a combination of uppercase letters, lower case letters, numbers, and special characters';
+
+const messageNetWorkErr = 'Vui Lòng Thử Lại, Khi Đường Truyền Ổn Định';
+
+const messageRegisterFailed = 'Đăng ký thất bại';
+
+const messageRegisterSucceed = 'Đăng ký thành công';
+
+const messageLoginFailed = 'Đăng nhập thất bại';
+
+const messageNameOrEmailIsExits = 'Tên hoặc email đã tồn tại';
+
+const messageNameRoomIsExits = 'Tên phòng  đã tồn tại';
+
+const messageUpdateFailed = 'Cập nhật thất bại';
+
+const messageUpdateSuccess = 'Cập nhật thành công';
+
+const messageIdIsUnValid = 'Id này ko có hoặc id ko hợp lệ';
+
+const messageEmailIsUnValid = '*Email is invalid!';
+
+const messageFieldIsRequire = '*Field is require!';
+
+const messageNameRoomShouldNotEmpty = '*Tên phòng không được để trống';
+
+const messageNameLocationShouldNotEmpty = '*Tên vị trí không được để trống';
+
+const messageNameUserShouldNotEmpty = '*Tên người dùng không được để trống';
+
+const messageNoDifferent = 'Không có thay đổi nào';
+
+const messagePlaceHolderSearchUser = 'Nhập vào tài khoản hoặc họ tên người dùng';
+
+const messagePlaceHolderSearchRoom = 'Nhập tên phòng';
+
+const messagePlaceHolderSearchLocation = 'Nhập vị trí';
+
+const messagePlaceHolderSearchEvaluate = 'Nhập nội dung đánh giá';
+
+const messagePlaceHolderSearchTicket = 'Nhập vé';
+
+const messageFailBooking = 'Đặt phòng thất bại';
+
+const messageWaringBooking = 'Vui lòng chọn ngày';
+
+const messageEvaluateSuccess = 'Tạo đánh giá thành công';
+
+const messageUpdateEvaluateSuccess = 'Cập nhật đánh giá thành công';
+
+const messageDeleteUserSuccess = 'Xóa người dùng thành công';
+
+const messageDeleteEvaluateSuccess = 'Xóa đánh giá thành công';
+
+const messageDeleteRoomSuccess = 'Xóa phòng thành công';
+
+const messageDeleteLocationSuccess = 'Xóa vị trí thành công';
+
+const messageDeleteTicketSuccess = 'Xóa vé thành công';
+
+const messageUpdateImageProfileSuccess = 'Xóa vé thành công';
+
+const messageAuthorization = 'You do not have permission to access this page';
+
+const messageUserNotLogin = 'Bạn cần phải đăng nhập để truy cập vào trang này';
+
+export const messageApp = {
+  messagePasswordIsValid,
+  messageNetWorkErr,
+  messageRegisterFailed,
+  messageLoginFailed,
+  messageRegisterSucceed,
+  messageNameOrEmailIsExits,
+  messageUpdateFailed,
+  messageIdIsUnValid,
+  messageUpdateSuccess,
+  messageNameRoomIsExits,
+  messageFieldIsRequire,
+  messageNameRoomShouldNotEmpty,
+  messageEmailIsUnValid,
+  messageNameLocationShouldNotEmpty,
+  messageNameUserShouldNotEmpty,
+  messageNoDifferent,
+  messagePlaceHolderSearchUser,
+  messagePlaceHolderSearchRoom,
+  messagePlaceHolderSearchLocation,
+  messageFailBooking,
+  messageWaringBooking,
+  messageEvaluateSuccess,
+  messageDeleteUserSuccess,
+  messageDeleteEvaluateSuccess,
+  messageDeleteRoomSuccess,
+  messageDeleteLocationSuccess,
+  messageDeleteTicketSuccess,
+  messagePlaceHolderSearchEvaluate,
+  messagePlaceHolderSearchTicket,
+  messageUpdateEvaluateSuccess,
+  messageUpdateImageProfileSuccess,
+  messageAuthorization,
+  messageUserNotLogin,
+};
